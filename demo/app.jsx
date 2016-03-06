9efff5127c8dfb6660e0a2e0477802bb89577bef
杨阳 <15682566795@163.com>
2016-03-06 13:28:23 +0800

    finish

--- demo/app.jsx
+++ demo/app.jsx
@@ -33,60 +33,143 @@
 // })
 //////////////
 
-var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+// var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
-var TodoList = React.createClass({
-  getInitialState: function() {
-    return {items: ['hello', 'world', 'click', 'me']};
-  },
-  handleAdd: function() {
-    var newItems =
-      this.state.items.concat([prompt('Enter some text')]);
-    this.setState({items: newItems});
-  },
-  handleRemove: function(i) {
+// var TodoList = React.createClass({
 
-    var newItems = this.state.items;
-    newItems.splice(i, 1);
-        console.log(newItems)
-    this.setState({items: newItems});
+//   getInitialState: function() {
+//     return {items: ['hello', 'world', 'click', 'me']};
+//   },
+//   handleAdd: function() {
+//     var newItems =
+//       this.state.items.concat([prompt('Enter some text')]);
+//     this.setState({items: newItems});
+//   },
+//   handleRemove: function(i) {
+
+//     var newItems = this.state.items;
+//     newItems.splice(i, 1);
+//         console.log(newItems)
+//     this.setState({items: newItems});
+//   },
+//   render: function() {
+//     var items = this.state.items.map(function(item, i) {
+//       return (
+//         <div key={item} onClick={this.handleRemove.bind(this, i)}>
+//           {item}
+//         </div>
+//       );
+//     }.bind(this));
+//     return (
+//       <div>
+//         <button onClick={this.handleAdd}>Add Item</button>
+//         <ReactCSSTransitionGroup transitionName="example">
+//           {items}
+//         </ReactCSSTransitionGroup>
+//       </div>
+//     );
+//   }
+// });
+// var WithLink = React.createClass({
+//   mixins: [React.addons.LinkedStateMixin],
+//   getInitialState: function() {
+//     return {message: 'Hello!',"message2":true,"message3":false,"message4":false};
+//   },
+//   getV:function(){
+//     console.log(this.state)
+//   },
+//   render: function() {
+//     return (
+//       <form action="">
+//       <input type="text" valueLink={this.linkState('message')} />
+//       <input type="checkbox" checkedLink={this.linkState('message2')} />
+//       <input type="radio" name='sex' checkedLink={this.linkState('message3')} />
+//       <input type="radio" name='sex' checkedLink={this.linkState('message4')} />
+//       </form>
+//       );
+//   }
+// });
+
+// React.render(<TodoList author='yang'/>,document.getElementById('container'))
+// var x= React.render(<WithLink author='yang'/>,document.getElementById('container2'));
+//////////////////
+
+// var str= '  aaa   ';
+// // // str.replace(/(^\s*)|(\s*$)/g,'');
+// // str.replace(/^(\s*)|(\s*)$/g, '');
+// var x =str.trim();
+// var arr=[1,2,3,4];
+// var x= arr.splice(1,2);
+// console.log(arr)
+// console.log(x);
+
+var Title=React.createClass({
+  componentWillReceiveProps:function(){
+    console.log('componentWillReceiveProps')
   },
-  render: function() {
-    var items = this.state.items.map(function(item, i) {
-      return (
-        <div key={item} onClick={this.handleRemove.bind(this, i)}>
-          {item}
-        </div>
-      );
-    }.bind(this));
+    componentWillUpdate:function(){
+    console.log('componentWillUpdate')
+  },
+    componentDidUpdate:function(){
+    console.log('componentDidUpdate')
+  },
+  render:function(){
+    console.log('render')
     return (
       <div>
-        <button onClick={this.handleAdd}>Add Item</button>
-        <ReactCSSTransitionGroup transitionName="example">
-          {items}
-        </ReactCSSTransitionGroup>
+    <h1>{this.props.item}</h1>
       </div>
-    );
+      )
   }
 });
-var WithLink = React.createClass({
-  mixins: [React.addons.LinkedStateMixin],
-  getInitialState: function() {
-    return {message: 'Hello!',"message2":true,"message3":false,"message4":false};
+var Content=React.createClass({
+  render:function(){
+    return (
+      <div>
+      <p>{this.props.item}</p>
+      </div>
+      )
+  }
+})
+var MainBody = React.createClass({
+  up:function(){
+    this.setState({
+      data:[{'title':'one','content':'one'}]
+    })
   },
-  getV:function(){
-    console.log(this.state)
+  componentWillMount:function(){
+    console.log('before')
+  },
+  getInitialState:function(){
+    console.log(this.props.data)
+    return {
+      data:this.props.data
+    }
   },
   render: function() {
+    var d = this.state.data.map(function(item,index){
+      return (
+        <div key={'index'+index}>
+        <Title item={item.title } />
+        <Content item={item.content } />
+        </div>);
+        })
     return (
-      <form action="">
-      <input type="text" valueLink={this.linkState('message')} />
-      <input type="checkbox" checkedLink={this.linkState('message2')} />
-      <input type="radio" name='sex' checkedLink={this.linkState('message3')} />
-      <input type="radio" name='sex' checkedLink={this.linkState('message4')} />
-      </form>
-      );
-  }
-});
-React.render(<TodoList author='yang'/>,document.getElementById('container'))
-var x= React.render(<WithLink author='yang'/>,document.getElementById('container2'))
+      <div>
+     { d }
+      </div>
+    )  
+
+}
+})
+var datae = [{
+  'title': 'one',
+  'content': 'one one one'
+}, {
+  'title': 'two',
+  'content': 'two two two'
+}, {
+  'title': 'three',
+  'content': 'three three three'
+}];
+var x =React.render(<MainBody data={datae}/>,document.getElementById('container'))
